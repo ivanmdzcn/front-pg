519ec15fb03a20448e52f2f90283a12b452f8c00
ivan <lesterivanmdz@gmail.com>
2025-09-07 00:49:36 -0600

    Correcion de variables de entorno

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://localhost:44312/api' //Local para pruebas IIS
-  apiUrl: 'http://192.168.1.24:8080/api' //API del Contenedor
+  //apiUrl: 'http://192.168.1.24:8080/api' //API del Contenedor
+  apiUrl: '/api' //Va hacia nginx.conf
 };
 
