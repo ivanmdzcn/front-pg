ee02076d57b9e2c443aef8a6d0b55e92ee56de42
ivan <lesterivanmdz@gmail.com>
2025-09-12 19:23:24 -0600

    Enviroment desarrollo

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44312/api' //Local para pruebas IIS
+  //apiUrl: 'https://localhost:44312/api' //Local para pruebas IIS Comentar cada vez de hacer commit
   //apiUrl: 'http://192.168.1.24:8080/api' //API del Contenedor
-  //apiUrl: '/api' //Va hacia nginx.conf
+  apiUrl: '/api' //Va hacia nginx.conf, descomentar cada vez que se hace commit
 };
 
